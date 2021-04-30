be2402fb51c506bd50ff9d1322d56e1ef0705242
csuryaprasad <csuryaprasad@gmail.com>
2021-04-30 14:17:10 -0400

    Intial Commit

---
+++ WithGradle/src/test/java/WithGradle/chromeExample.java
@@ -0,0 +1,21 @@
+package WithGradle;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chromeExample {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\ChromeDriver\\chromedriver.exe");  
+		WebDriver driver=new ChromeDriver();  
+		
+        driver.get("http://www.facebook.com");
+        
+        WebElement mailID = driver.findElement(By.id("email"));
+        mailID.sendKeys("test1@gmail.com");
+        
+        driver.close();
+	}
+}
